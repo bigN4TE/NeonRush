b053435092c9b1823a789aa96f5d8dbdb85b1935
bigN4TE <nathanel.col@gmail.com>
2024-05-02 11:33:12 +0400

    refonte du code, ajout du système de tiles, modification du sytème de mouvement

--- NeonRun.py
+++ NeonRun.py
@@ -9,9 +9,65 @@ screen_height = 1080
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Neon Run')
 
+#tile size
+tile_size = 128
+
 #background
-fond = pygame.image.load("NRR background1.png").convert()
-screen.blit(fond, (0,0))
+fond = pygame.image.load("background proto.png")
+
+#tile grid
+def draw_grid():
+    for line in range(2, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+#tile system
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        #tile assets
+        stage_img = pygame.image.load('img/2.png')
+        speaker_img = pygame.image.load('img/5.png')
+
+        #tile value
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(stage_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(speaker_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+#tile placement
+world_data = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
 #character
 class Player():
@@ -19,55 +75,66 @@ class Player():
         perso = pygame.image.load("NRR still test.png").convert_alpha()
         self.image = pygame.transform.scale(perso, (192, 192))
         self.rect = self.image.get_rect()
-        self.velocity_x = 0
-        self.velocity_y = 0
-    
-    def move(self):
-        if player.velocity_y > 0:
-            if player.rect.bottom + player.velocity_y > screen_height:
-                player.velocity_y = 0
-        if player.velocity_y < 0:
-            if player.rect.top + player.velocity_y <= 0:
-                player.velocity_y = 0
-        if player.velocity_x > 0:
-            if player.rect.right + player.velocity_x > screen_width:
-                player.velocity_x = 0
-        if player.velocity_x + player.velocity_x < 0:
-            if player.rect.left <= 0:
-                player.velocity_x = 0
-        self.rect.x += self.velocity_x
-        self.rect.y += self.velocity_y
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
 
     def update(self):
+        dx = 0
+        dy = 0
+
+    #controles
+        key = pygame.key.get_pressed()
+        if key[pygame.K_UP] and self.jumped == False:
+            self.velocity_y = -10
+            self.jumped = True
+        if key[pygame.K_UP] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+        if key[pygame.K_RIGHT]:
+            dx += 5
+        
+        #gravity
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        #coordinates update
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            dy = 0
+
         screen.blit(self.image, self.rect)
 
 pygame.display.flip()
 
-player = Player(100, screen_height - 335)
 
-run = 1
+#Loop
+player = Player(100, screen_height - 335)
+world = World(world_data)
+
+run = True
 pygame.key.set_repeat(10,10)
 while run:
 
     player.update()
 
+    screen.blit(fond, (0,0))
+
+    world.draw()
+
+    draw_grid()
+
     for event in pygame.event.get():
         if event.type == KEYDOWN and event.key == K_ESCAPE:
-            run = 0
-        elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.velocity_x = -3
-                elif event.key == pygame.K_RIGHT:
-                    player.velocity_x = 3
-                elif event.key == pygame.K_UP:
-                    player.velocity_y = -3
-                elif event.key == pygame.K_DOWN:
-                    player.velocity_y = 3
-        elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    player.velocity_x = 0
-                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    player.velocity_x = 0
+            run = False
 
     pygame.display.update()
-    
+
+pygame.quit()
